2869a4c6465d6c708a560ec791ee07c251e9d1ce
Silvio Silva <silvio@geslab>
2021-03-08 00:19:47 -0300

    findByID busca por id

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -10,15 +10,12 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Livros"); 
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 7600.20, obj);
-		
-		System.out.println(seller);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-
+	
+		Seller seller = sellerDao.findById(5);
+		
+		System.out.println(seller);
 	}
 
 }
