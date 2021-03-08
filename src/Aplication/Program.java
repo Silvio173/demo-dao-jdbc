944c658ab4043128ccc41b96044dedc074695804
Silvio Silva <silvio@geslab>
2021-03-08 07:29:44 -0300

    Inserir dados com insert

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -1,5 +1,6 @@
 package Aplication;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -18,14 +19,28 @@ public class Program {
 		System.out.println(seller);
 		
 		System.out.println();
-		System.out.println();
 		
 		System.out.println("====== Teste 2 = sellerByDepartment=========");
 			Department department = new Department(2, null);
 			List<Seller> list = sellerDao.findByDepartment(department);
 			for(Seller obj: list) {
 				System.out.println(obj);
-			}
 		}
 
+			System.out.println();
+			
+			System.out.println("====== Teste 4 = sellerInsert=========");
+			Seller newseller = new Seller(null, "Helena Oliveira", "helena@uol.com.br", new Date(), 17100.50, department);
+			sellerDao.insert(newseller);
+			System.out.println("Inseriu novo ID = " + newseller.getId());
+			
+			System.out.println();
+			
+			System.out.println("====== Teste 3 = sellerFindAll=========");
+			List<Seller> lista = sellerDao.findAll();
+				for(Seller obj: lista) {
+					System.out.println(obj);
+				}
+
+	}
 }
--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,8 +26,37 @@ public class SellerDaoJdbc implements SellerDao{
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email,BirthDate,BaseSalary,DepartmentId) "
+					+ "VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
 		
+		st.setString(1, obj.getName());	
+		st.setString(2, obj.getEmail());
+		st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+		st.setDouble(4, obj.getBaseSalary());
+		st.setInt(5, obj.getDepartment().getId());
+		
+		int rowsAffected = st.executeUpdate();
+		
+		if(rowsAffected > 0) {
+			ResultSet rs = st.getGeneratedKeys();
+			if(rs.next()) {
+				int id = rs.getInt(1);
+				obj.setId(id);
+			}
+			DB.closeResultSet(rs);
+		}else {
+			throw new DbException("Nenhuma linha foi alterada");
+		}
+		
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -84,8 +114,38 @@ public class SellerDaoJdbc implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null; 
+		
+		try {
+			st = conn.prepareStatement(
+					"select seller.*, department.Name as DepName "
+					+ "from seller inner join department "
+					+ "on seller.DepartmentId = department.Id ");
+			
+			rs = st.executeQuery();
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer,Department> map = new HashMap<>();
+	
+			
+			while(rs.next()) {
+				Department dep = map.get(rs.getInt("Departmentid"));
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("Departmentid"), dep);	
+				}
+				Seller obj = instantiateSeller(rs, dep); 
+				list.add(obj);
+			}
+			return list;
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+
 	}
 
 	@Override
