9a1eeb99531979cd00547259421fadb2abe1d5a5
Silvio Silva <silvio@geslab>
2021-03-07 23:22:33 -0300

    DaoFactory e SellerDao

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -2,6 +2,8 @@ package Aplication;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 7600.20, obj);
 		
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
