8a705a67a83b7c94f7814c57a70d748928c575d3
Silvio Silva <silvio@geslab>
2021-03-08 00:30:01 -0300

    findByID busca por id modificado program

--- src/Aplication/Program.java
+++ src/Aplication/Program.java
@@ -13,8 +13,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 	
+		System.out.println("====== Teste 1 = sellerById =========");
 		Seller seller = sellerDao.findById(5);
-		
 		System.out.println(seller);
 	}
 
